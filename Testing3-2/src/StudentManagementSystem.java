72f8a893359e272af6e6bbfcf0a7960d7d61e4b8
VamshiYadav45 <vamshibanka2006@gmail.com>
2025-12-27 10:54:12 +0530

    Update StudentManagementSystem.java

--- Testing3-2/src/StudentManagementSystem.java
+++ Testing3-2/src/StudentManagementSystem.java
@@ -1,15 +1,11 @@
 package project1;
 import java.util.Scanner;
 public class StudentManagementSystem {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int n = 7;
         String[] names = new String[n];
         int[] marks = new int[n];
-
         // 1. Input student names and marks
         System.out.println("Enter names and marks of 10 students:");
         for (int i = 0; i < n; i++) {
@@ -20,7 +16,6 @@ public class StudentManagementSystem {
             marks[i] = sc.nextInt();
             sc.nextLine(); // consume newline
         }
-
         // 2. Find Topper
         int max = marks[0];
         int topperIndex = 0;
@@ -31,15 +26,12 @@ public class StudentManagementSystem {
                 topperIndex = i;
             }
         }
-
         System.out.println("\nTopper:");
         System.out.println(names[topperIndex] + " with marks " + marks[topperIndex]);
-
         // 3. Search student by name
         System.out.print("\nEnter student name to search: ");
         String searchName = sc.nextLine();
         boolean found = false;
-
         for (int i = 0; i < n; i++) {
             if (names[i].equalsIgnoreCase(searchName)) {
                 System.out.println("Student Found: " + names[i] + " - Marks: " + marks[i]);
@@ -47,11 +39,9 @@ public class StudentManagementSystem {
                 break;
             }
         }
-
         if (!found) {
             System.out.println("Student not found.");
         }
-
         // 4. Display students with marks > 75
         System.out.println("\nStudents with marks greater than 75:");
         for (int i = 0; i < n; i++) {
@@ -59,26 +49,20 @@ public class StudentManagementSystem {
                 System.out.println(names[i] + " - " + marks[i]);
             }
         }
-
         // 5. Calculate class average
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += marks[i];
         }
-
         double average = (double) sum / n;
         System.out.println("\nClass Average: " + average);
-
         // 6. Sort students by marks (ascending order)
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (marks[i] > marks[j]) {
-                    // swap marks
                     int tempMarks = marks[i];
                     marks[i] = marks[j];
                     marks[j] = tempMarks;
-
-                    // swap corresponding names
                     String tempName = names[i];
                     names[i] = names[j];
                     names[j] = tempName;
@@ -94,4 +78,5 @@ public class StudentManagementSystem {
 
         sc.close();
     }
+
 }
